4cfcc7b2ef48115dea7d8ebdf22370d76025abcf
Nikita Pilinka <nikita.pilinko@sunmait.com>
2019-12-01 19:03:23 +0300

    Updates

---
+++ precache-manifest.959f64c69a0e70b41354839dbb8d551d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c8437d287ea34b27a2cf5f57b6160f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbb4e9933a9c32db94d3",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "41edd9245697bdeddc17",
+    "url": "/static/css/main.100b00d3.chunk.css"
+  },
+  {
+    "revision": "dbb4e9933a9c32db94d3",
+    "url": "/static/js/2.d2455ba3.chunk.js"
+  },
+  {
+    "revision": "41edd9245697bdeddc17",
+    "url": "/static/js/main.17da46c5.chunk.js"
+  },
+  {
+    "revision": "b383ff72251a3fdcb5d9",
+    "url": "/static/js/runtime-main.a420b92f.js"
+  },
+  {
+    "revision": "e4b2946953de6d9646a52a635aef99f7",
+    "url": "/static/media/logo.e4b29469.png"
+  },
+  {
+    "revision": "d75d7f70aa160104a45a743eae7a8a7e",
+    "url": "/static/media/бляскийШрифт.d75d7f70.otf"
+  }
+]);
