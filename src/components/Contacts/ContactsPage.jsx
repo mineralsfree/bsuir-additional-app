1962010f1a231e58291152518283add85ddb245a
Alexander Pankratiev <a.pankratiev@syberry.com>
2020-05-04 21:44:46 +0300

    Contact page

---
+++ src/components/Contacts/ContactsPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const ContactsPage = () => (
+    <div className="contacts-wrapper">
+        <div className="contacts">
+            <h1>Контактная информация</h1>
+            <p>
+                При при использовании сайта у Вас возникла проблема или вопрос, смело обращайтесь на email разработчиков:
+            </p>
+            <ul>
+                <li>sasha.pankratiew@gmail.com (backend developer)</li>
+                <li>mineralsfree@gmail.com (frontend developer)</li>
+                <li>nikto.a.kto@gmail.com (mobile developer)</li>
+            </ul>
+        </div>
+    </div>
+);
+
