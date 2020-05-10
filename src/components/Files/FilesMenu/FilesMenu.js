877b50dafa698783f3acb24fc9d2a66271172989
Alexander Pankratiev <a.pankratiev@syberry.com>
2020-05-10 11:26:29 +0300

    Fixed files downloading

--- src/components/Files/FilesMenu/FilesMenu.js
+++ src/components/Files/FilesMenu/FilesMenu.js
@@ -2,25 +2,29 @@ import React from 'react';
 import {Menu, Item,} from 'react-contexify';
 import 'react-contexify/dist/ReactContexify.min.css';
 import {filesApi} from "../../../api/filesApi";
-import fileDownload from 'js-file-download'
 import {useDispatch} from "react-redux";
 import {filesActions} from "../../../redux/files/filesSlice";
 import {useSelector} from "react-redux";
 import {getCurrentDirSelector} from "../../../redux/files/filesSelectors";
+import {saveAs} from "file-saver";
+
 export const FilesMenu = props => {
     const dispatch = useDispatch();
     const dir = useSelector(getCurrentDirSelector);
+
     const onDownload = ({event, props}) => {
         filesApi.downloadFile(props.id)
-            .then((response) => {
-                fileDownload(response.data, props.fileName)
+            .then(({data}) => {
+                saveAs(new Blob([data]), props.fileName);
             })
-    }
+    };
+
     const onDelete = ({event, props}) => {
         filesApi.deleteFile(props.id).then((response) => {
             dispatch(filesActions.getDir(dir))
         })
     };
+
     return (
         <Menu id='menu_id'>
             <Item onClick={onDownload}>Download</Item>
@@ -28,4 +32,4 @@ export const FilesMenu = props => {
         </Menu>
     );
 
-}
+};
